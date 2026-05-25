9a8c993c8e35d3edccd81b8ec06d35e771ffa533
PlayErsGames666 <playersgames.ru@gmail.com>
2026-05-25 17:48:32 +0500

    Create first page with html code

--- Rising/main/views.py
+++ Rising/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse # Конвертрует html code для браузера
 
-def index():
-    pass
+def index(request): # Обязательно нужно получать хотя бы один параметр, иначе не будет работать
+    return HttpResponse("<h4>Дорога работает</h4>") # Конвертрует html code для браузера
 
 
