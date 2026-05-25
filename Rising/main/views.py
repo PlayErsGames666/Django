6a6131b820f6ace2dd836c263b26aabd427f4a50
PlayErsGames666 <playersgames.ru@gmail.com>
2026-05-25 17:59:43 +0500

    Update HTML

--- Rising/main/views.py
+++ Rising/main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse # Конвертрует html code для браузера
 
 def index(request): # Обязательно нужно получать хотя бы один параметр, иначе не будет работать
-    return HttpResponse("<h4>Дорога работает</h4>") # Конвертрует html code для браузера
+    return HttpResponse("<h4>Дорога работает</h4>" \
+    "<a href=/about>About U</a>") # Конвертрует html code для браузера
 
-
+def about(request):
+    return HttpResponse("<h4>About your mother LS</h4>")
