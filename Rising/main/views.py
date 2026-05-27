a098084acb6ef772937241d7542f1ad3073bc516
PlayErsGames666 <playersgames.ru@gmail.com>
2026-05-27 18:10:04 +0500

    Update structure, make it more optimal and expandable

--- Rising/main/views.py
+++ Rising/main/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse # Конвертрует html code для �
 def index(request):
     return render(request, 'main/index.html')
 
-def main(request): # Обязательно нужно получать хотя бы один параметр, иначе не будет работать
-    return HttpResponse("<h4>Дорога работает</h4>" \
-    "<a href=/about>About U<br></a>" \
-    "<a href=/index>Index</a>") # Конвертрует html code для браузера
+# Старый способ вызозова HTML
+# def main(request): # Обязательно нужно получать хотя бы один параметр, иначе не будет работать
+#     return HttpResponse("<h4>Дорога работает</h4>" \
+#     "<a href=/index>Index</a>") # Конвертрует html code для браузера
 
 def about(request):
-    return HttpResponse("<h4>About your mother LS</h4>")
+    return render(request, 'main/about.html')
