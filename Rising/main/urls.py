383cc9d85f66cc713cd2bb8773f2ea8cedc00f54
PlayErsGames666 <playersgames.ru@gmail.com>
2026-05-27 18:08:44 +0500

    Update urls for make the way for link about

--- Rising/main/urls.py
+++ Rising/main/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.main),
-    path('index', views.index),
-    path('about', views.about)
+    path('', views.index),
+    path('about/', views.about)
 ]
