ecd5deb3408084f19462021e23302611b6f8f8b4
PlayErsGames666 <playersgames.ru@gmail.com>
2026-05-27 17:50:03 +0500

    Update break for index link

--- Rising/main/views.py
+++ Rising/main/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse # Конвертрует html code для браузера
 
 def index(request):
-    return render(request, '')
+    return render(request, 'main/index.html')
 
 def main(request): # Обязательно нужно получать хотя бы один параметр, иначе не будет работать
     return HttpResponse("<h4>Дорога работает</h4>" \
-    "<a href=/about>About U</a>") # Конвертрует html code для браузера
+    "<a href=/about>About U<br></a>" \
+    "<a href=/index>Index</a>") # Конвертрует html code для браузера
 
 def about(request):
     return HttpResponse("<h4>About your mother LS</h4>")
