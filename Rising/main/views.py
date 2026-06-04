c355c4c93d5012665f813fcc23557057f8009dd2
PlayErsGames666 <playersgames.ru@gmail.com>
2026-06-04 21:01:51 +0500

    Add to render, broadcasting

--- Rising/main/views.py
+++ Rising/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 # from django.http import HttpResponse # Конвертрует html code для браузера
 
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', {'title': 'Main Page'})
 
 # Старый способ вызозова HTML
 # def main(request): # Обязательно нужно получать хотя бы один параметр, иначе не будет работать
@@ -10,4 +10,4 @@ def index(request):
 #     "<a href=/index>Index</a>") # Конвертрует html code для браузера
 
 def about(request):
-    return render(request, 'main/about.html')
+    return render(request, 'main/about.html', {'about': 'About Us'})
