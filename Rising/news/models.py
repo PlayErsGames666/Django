c2d75de1a9624494bd2c05263d957536633be490
PlayErsGames666 <playersgames.ru@gmail.com>
2026-06-06 20:03:40 +0500

    Create table to data base

--- Rising/news/models.py
+++ Rising/news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model): # Создаём класс как таблицу в бд
+    title = models.CharField('Title', max_length=50) # Добавляем название и максимальную длинну 50 символов
+    announs = models.CharField('The Announcement', max_length=250) # Добавляем название и максимальную длинну 250 символов
+    text = models.TextField() # Здесь уже используется метод textfield, в нём может быть от 1 000 символов и больше
+    date = models.DateTimeField() # Добавляем дату и время
+
+    def __str__(self): # На след уроке объяснит
+        return self.title
