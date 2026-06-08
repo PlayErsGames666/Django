9fdb597f7af480562adc57d69f40d97677ff8283
PlayErsGames666 <playersgames.ru@gmail.com>
2026-06-08 20:00:54 +0500

    add functional text

--- Rising/news/models.py
+++ Rising/news/models.py
@@ -6,8 +6,8 @@ class Articles(models.Model): # Создаём класс как таблицу 
     text = models.TextField() # Здесь уже используется метод textfield, в нём может быть от 1 000 символов и больше
     date = models.DateTimeField() # Добавляем дату и время
 
-    def __str__(self): # На след уроке объяснит
-        return self.title
+    def __str__(self): # Крч, выводит и название статьи, если его не будет то просто будет выводить id статью
+        return f'News: {self.title}' # Добавляет к началу строки слово News
 
     class Meta: # Создали класс для смены названия в админ панели
         verbose_name = "New" # Назваение таблицы в ед числе
