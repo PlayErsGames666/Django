b8c0fcb232959bee8b8a9d1cc43036211074ee7b
PlayErsGames666 <playersgames.ru@gmail.com>
2026-06-08 20:14:46 +0500

    Update structure

--- Rising/news/views.py
+++ Rising/news/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from .models import Articles
 
 def news(request):
-    news = Articles.objects.all() # Создали переменную с данными из таблицы Articles. all() означает все данные
+    news = Articles.objects.order_by('date') # Создали переменную с данными из таблицы Articles. all() означает все данные
+    # Если поставить - перед title сортирует в обратном порядке
+    # [:2] указывает количество блоков
     return render(request, 'news/news.html', {'news': news}) # Добавили ключ для работы с articles
