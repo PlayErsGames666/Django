563a0d70959ebaf9fdd2dc3cf8c9df2f54510797
PlayErsGames666 <playersgames.ru@gmail.com>
2026-06-08 19:29:37 +0500

    Add comment

--- Rising/news/views.py
+++ Rising/news/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Articles
 
 def news(request):
-    news = Articles.objects.all()
-    return render(request, 'news/news.html', {'news': news})
+    news = Articles.objects.all() # Создали переменную с данными из таблицы Articles. all() означает все данные
+    return render(request, 'news/news.html', {'news': news}) # Добавили ключ для работы с articles
