85567b0c159d12e4220e6d5b25fb55dbcb716931
PlayErsGames666 <playersgames.ru@gmail.com>
2026-06-13 23:01:13 +0500

    make HTTP protocol for checking POST query

--- Rising/news/views.py
+++ Rising/news/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
 
 def news(request):
-    news_list = Articles.objects.order_by('date') # Создали переменную с данными из таблицы Articles. all() означает все данные
+    news_list = Articles.objects.all() # Создали переменную с данными из таблицы Articles. all() означает все данные
     # Если поставить - перед title сортирует в обратном порядке
     # [:2] указывает количество блоков
     data = {
@@ -13,9 +13,19 @@ def news(request):
     return render(request, 'news/news.html', data) # Добавили ключ для работы с articles
 
 def create(request):
+    error = ""
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news')
+        else:
+            error = '4xx error'
+
     form = ArticlesForm()
     data = {
         'title_name': 'Create Posts',
-        'form': form
+        'form': form,
+        'error': error
     }
     return render(request, 'news/create.html', data)
