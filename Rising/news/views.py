0bfd01374c8ac6ac0f165c4cc3db9002e4b14337
PlayErsGames666 <playersgames.ru@gmail.com>
2026-06-13 22:00:49 +0500

    Add new def fucntion and update previous one with new transfer

--- Rising/news/views.py
+++ Rising/news/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from .models import Articles
 
 def news(request):
-    news = Articles.objects.order_by('date') # Создали переменную с данными из таблицы Articles. all() означает все данные
+    news_list = Articles.objects.order_by('date') # Создали переменную с данными из таблицы Articles. all() означает все данные
     # Если поставить - перед title сортирует в обратном порядке
     # [:2] указывает количество блоков
-    return render(request, 'news/news.html', {'news': news}) # Добавили ключ для работы с articles
+    data = {
+        'news': 'News' ,
+        'news': news_list
+    }
+    return render(request, 'news/news.html', data) # Добавили ключ для работы с articles
+
+def create(request):
+    data = {
+        'title_name': 'Create Posts'
+    }
+    return render(request, 'news/create.html', data)
