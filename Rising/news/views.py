14117ccbfae62c6bbcedd8c266d33becb2311eb2
PlayErsGames666 <playersgames.ru@gmail.com>
2026-06-14 20:37:04 +0500

    add 3 other CRUD class Read, Update, Delete

--- Rising/news/views.py
+++ Rising/news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 def news(request):
     news_list = Articles.objects.all() # Создали переменную с данными из таблицы Articles. all() означает все данные
@@ -18,6 +18,17 @@ class NewDetailView(DetailView):
     template_name = 'news/details_view.html'
     context_object_name = 'article'
 
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+
+    form_class = ArticlesForm
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news/'
+    template_name = 'news/news-delete.html'
 
 def create(request):
     error = ""
