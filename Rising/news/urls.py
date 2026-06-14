4c9058b7ac21fd3cfab5b0a078d2fe9b56bff54b
PlayErsGames666 <playersgames.ru@gmail.com>
2026-06-14 20:32:45 +0500

    create a path for updating and deleting post data

--- Rising/news/urls.py
+++ Rising/news/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.news, name = 'news'),
     path('create/', views.create, name = 'create'),
-    path('<int:pk>', views.NewDetailView.as_view(), name = 'news-detail')
+    path('<int:pk>', views.NewDetailView.as_view(), name = 'news-detail'), # Создали динамическую ссылку, чтобы каждый id поста можно было просмотреть по отдельности
+    path('<int:pk>/update', views.NewsUpdateView.as_view(), name = 'news-update'), # Создали ссылку для обновление страницы
+    path('<int:pk>/delete', views.NewsDeleteView.as_view(), name = 'news-delete')
 ]
